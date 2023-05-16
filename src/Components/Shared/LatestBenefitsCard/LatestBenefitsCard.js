9cce8ba96400e3b9f24e020bb41a52bb474b7260
simeon.rudashki <simeon.rudashki@abv.bg>
2023-05-16 17:16:53 +0300

    latest benefits refactored

--- src/Components/Shared/LatestBenefitsCard/LatestBenefitsCard.js
+++ src/Components/Shared/LatestBenefitsCard/LatestBenefitsCard.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-import styles from './styles/latestBenefitsCard.module.css';
-
 function LatestBenefitsCard({
     img
 }) {
     return (
-        <article className={styles['card']}>
-            <div className={styles['card__img-wrapper']}>
-                <img src={img} alt="first" />
+        <article className='w-full h-auto shadow-lg xl:w-[33%] xl:h-[100%]'>
+            <div className='w-full h-[245px] flex justify-center items-center mb-5'>
+                <img className='w-full h-full object-cover' src={img} alt="first" />
             </div>
 
-            <div className={styles['card__desc']}>
-                <p>Category: name</p>
-                <h4>The quick brown fox...</h4>
+            <div className='font-poppins xl:text-left text-center ml-4'>
+                <p className='text-[13px] text-[#707c89] mb-3'>Category: name</p>
+                <h4 className='xl:text-xl text-[#212832] mb-5'>The quick brown fox...</h4>
 
-                <button type='button'>Read article</button>
+                <button
+                    className='mb-4 bg-mainBlueColor py-3 px-4 cursor-pointer 
+                    border-none text-mainWhiteColor hover:bg-hoverBlueColor transition-all'
+                    type='button'>
+                    Read article
+                </button>
             </div>
         </article>
     )
